bc5e51e0037c07f5e2d7af40cb97eec282a4093c
Swasthik Prabhu <swasthikp03@gmail.com>
2025-06-02 19:26:44 +0530

    added auth

---
+++ server/core/security.py
@@ -0,0 +1,35 @@
+from datetime import datetime, timedelta
+from typing import Optional
+
+from jose import JWTError, jwt
+from passlib.context import CryptContext
+from .config import settings
+
+pwd_context = CryptContext(schemes=["bcrypt"], deprecated="auto")
+ALGORITHM = settings.JWT_ALGORITHM
+SECRET_KEY = settings.JWT_SECRET_KEY
+ACCESS_TOKEN_EXPIRE_MINUTES = settings.ACCESS_TOKEN_EXPIRE_MINUTES
+
+
+def verify_password(plain_password: str, hashed_password: str) -> bool:
+    return pwd_context.verify(plain_password, hashed_password)
+
+
+def get_password_hash(password: str) -> str:
+    return pwd_context.hash(password)
+
+
+def create_access_token(data: dict, expires_delta: Optional[timedelta] = None):
+    to_encode = data.copy()
+    expire = datetime.utcnow() + (expires_delta or timedelta(minutes=ACCESS_TOKEN_EXPIRE_MINUTES))
+    to_encode.update({"exp": expire})
+    encoded_jwt = jwt.encode(to_encode, SECRET_KEY, algorithm=ALGORITHM)
+    return encoded_jwt
+
+
+def decode_access_token(token: str):
+    try:
+        payload = jwt.decode(token, SECRET_KEY, algorithms=[ALGORITHM])
+        return payload
+    except JWTError:
+        return None
---
+++ server/models/user.py
@@ -0,0 +1,19 @@
+from beanie import Document
+from pydantic import EmailStr, Field
+from typing import Literal
+from datetime import datetime
+from core.security import verify_password
+
+
+class User(Document):
+    name: str
+    email: EmailStr
+    hashed_password: str
+    role: Literal["student", "admin","institution"] = "student"
+    created_at: datetime = Field(default_factory=datetime.utcnow)
+
+    class Settings:
+        name = "users"
+
+    async def verify_password(self, plain_password: str) -> bool:
+        return verify_password(plain_password, self.hashed_password)
